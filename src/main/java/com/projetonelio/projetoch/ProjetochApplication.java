c347df671b510ed752897ca26959241ae987b824
Carlos Heitor <carlosheitorjesus80@gmail.com>
2026-05-28 14:22:20 -0300

    Project created

---
+++ src/main/java/com/projetonelio/projetoch/ProjetochApplication.java
@@ -0,0 +1,13 @@
+package com.projetonelio.projetoch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetochApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetochApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetonelio/projetoch/ProjetochApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetonelio.projetoch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetochApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
