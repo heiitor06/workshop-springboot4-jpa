d902138b16c81c8122699a4490d7dde4b0de002d
Carlos Heitor <carlosheitorjesus80@gmail.com>
2026-05-28 17:21:16 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projetonelio/projetoch/entities/User.java
+++ src/main/java/com/projetonelio/projetoch/entities/User.java
@@ -1,14 +1,21 @@
 package com.projetonelio.projetoch.entities;
 
+
+
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
